a81dd37e4ca0ffd020bfc6b2767194e5cc44015f
PradumnKala <pradumnkalalp@gmail.com>
2025-03-15 02:21:06 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -426,12 +426,12 @@ function sendToWhatsApp() {
   }
   
   // right click block
-  document.addEventListener('contextmenu', function(event) {
-	event.preventDefault();
-  });
-  document.addEventListener('keydown', function(event) {
-	if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I") || (event.ctrlKey && event.key === "U")) {
-	  event.preventDefault();
-	}
-  });
+ //  document.addEventListener('contextmenu', function(event) {
+	// event.preventDefault();
+ //  });
+ //  document.addEventListener('keydown', function(event) {
+	// if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I") || (event.ctrlKey && event.key === "U")) {
+	//   event.preventDefault();
+	// }
+ //  });
   
